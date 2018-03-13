474302f4f19729d6fc7c8057d392c0d33869096d
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-13 16:15:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,9 +19,9 @@ class Song
     @artist = artist
   end
 
-  def self.reset_all
-    all.clear
-  end
+  # def self.reset_all
+  #   all.clear
+  # end
 
 
 
@@ -29,9 +29,9 @@ class Song
     name.downcase.gsub(' ', '-')
   end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
 
   def self.all
