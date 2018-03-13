c0ca1f1abc0dd174efecca3d274d50e86c3a0c3f
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-13 16:27:58 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -8,9 +8,9 @@ module Memorable
     all.count
   end
 
-  def find_by_name(name)
-    all.detect{|a| a.name == name}
-  end
+  # def find_by_name(name)
+  #   all.detect{|a| a.name == name}
+  # end
 
 
 
